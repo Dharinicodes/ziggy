62b036722d4c823dc1e25531e5a1cff9140e401d
Dharinicodes <126567741+Dharinicodes@users.noreply.github.com>
2023-07-31 11:45:14 +0530

    hoc part-2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,30 +1,28 @@
 import { CDN_URL } from "../utils/constants";
 
 const ItemList = ({ items }) => {
-  console.log(items);
-
   return (
     <div>
-      {" "}
       {items.map((item) => (
         <div className="accordion-body" key={item.card.info.id}>
-          <div>
-          <div>
-            <span>{item.card.info.name} </span>
-            <span>
-              {" "}
-              - ₹{" "}
-              {item.card.info.price
-                ? item.card.info.price / 100
-                : item.card.info.defaultPrice / 100}{" "}
-            </span>
+          <div className="dish-container">
+            <div>
+              <span>{item.card.info.name} </span>
+              <span>
+                {" "}
+                - ₹{" "}
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}{" "}
+              </span>
+            </div>
+            <p className="dish-description">{item.card.info.description}</p>
           </div>
-          <p>{item.card.info.description}</p>
-          </div>
           <div>
-          <img src={CDN_URL + item.card.info.imageId} /> </div>
+            <img src={CDN_URL + item.card.info.imageId} className="dish-pic" />{" "}
+          </div>
         </div>
-      ))}{" "}
+      ))}
     </div>
   );
 };
